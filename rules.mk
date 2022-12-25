ce34e8f02663788e8645fe17d9d18540ae62db61
Marcel van der Boom <marcel@hsdev.com>
2022-12-25 16:19:53 +0100

    Bring in dynamic macros for the remaining two macro keys
    So, 3 predefined macro keys and 2 definable in use.
    
    - updated the layout picture
    - empty macro.c source file, we dont use specifics yet.

--- rules.mk
+++ rules.mk
@@ -1,6 +1,8 @@
 LTO_ENABLE		= yes
 KEY_OVERRIDE_ENABLE	= yes
 AUTO_SHIFT_ENABLE	= yes
+DYNAMIC_MACRO_ENABLE 	= yes
+
 CONSOLE_ENABLE		= no
 COMMAND_ENABLE		= no
 MOUSEKEY_ENABLE	= no
@@ -19,4 +21,8 @@ endif
 
 ifeq ($(strip $(AUTO_SHIFT_ENABLE)), yes)
 	SRC += src/autoshift.c
+endif
+
+ifeq ($(strip $(DYNAMIC_MACRO_ENABLE)), yes)
+	SRC += src/macro.c
 endif
---
+++ src/macro.c
@@ -0,0 +1,12 @@
+#include QMK_KEYBOARD_H
+
+/* Macro related functions.
+
+   There are 2 places dealing with macros now:
+
+   1. The keymap defines the macros 0,1 and 2 to have predefined values
+
+   2. macros 3 and 4 are dynamic macros, no default value
+
+
+*/
