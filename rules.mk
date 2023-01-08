0ef401825eb8f0bfc4bfa8fe3b2e85a1866f2c25
Marcel van der Boom <marcel@hsdev.com>
2023-01-08 15:25:26 +0100

    Layout and some extra info, new TODO

--- rules.mk
+++ rules.mk
@@ -1,12 +1,20 @@
+# LTO = link time optimization, reduces size
 LTO_ENABLE		= yes
+
+# Overriding keys to produce another keycode
+# Need this for my parenthesis/braces/hooks config
 KEY_OVERRIDE_ENABLE	= yes
+
+# Automatically used shifted keycode on hold, timeout in config.h
 AUTO_SHIFT_ENABLE	= yes
+
+# Allow recording macros (2 keys currently)
 DYNAMIC_MACRO_ENABLE 	= yes
 
 CONSOLE_ENABLE		= no
 COMMAND_ENABLE		= no
-MOUSEKEY_ENABLE	= no
-EXTRAKEY_ENABLE	= no
+MOUSEKEY_ENABLE		= no
+EXTRAKEY_ENABLE		= no
 SPACE_CADET_ENABLE	= no
 GRAVE_ESC_ENABLE	= no
 MAGIC_ENABLE		= no
