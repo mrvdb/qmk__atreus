0dab1e309f7fabc01763cb523ff0a8519d2611cd
Marcel van der Boom <marcel@hsdev.com>
2023-01-31 10:41:14 +0100

    Make keys in comment clearer

--- src/override.c
+++ src/override.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-/* change Shift-( and Shift-) to emit < and > */
+/* change 'Shift-(' and 'Shift-)' to emit '<' and '>' */
 const key_override_t lprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_LT);
 const key_override_t rprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, KC_GT);
 
